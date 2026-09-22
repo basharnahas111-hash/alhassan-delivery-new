98d5b9a85a4ba57fdc0493aa03e4400da1a0362d
basharnahas111-hash <bashar.nahas111@gmail.com>
2026-09-22 13:41:50 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -6,6 +6,28 @@ export default {
       return new Response("WORKER OK");
     }
 
-    return env.ASSETS.fetch(request);
+    if (url.pathname === "/api/shops") {
+      if (request.method === "GET") {
+        const data = await env.ALHASSAN_DATA.get("shops");
+
+        return new Response(data || "[]", {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        });
+      }
+
+      if (request.method === "POST") {
+        const data = await request.text();
+
+        await env.ALHASSAN_DATA.put("shops", data);
+
+        return new Response("OK");
+      }
+
+      return new Response("Method Not Allowed", { status: 405 });
+    }
+
+    return new Response("ALHASSAN API WORKER OK");
   }
 };
